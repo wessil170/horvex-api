000cb36f827dede744962322712950f0e71d5109
Wesley Pereira <wessil170@gmail.com>
2026-02-18 15:29:14 -0300

    remove proxy middleware

--- app/main.py
+++ app/main.py
@@ -1,24 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 from app.database import engine, Base
 from app import models
 from app.routes import salons, clientes, servicos, agendamentos, auth
 
 app = FastAPI()
-
-# =========================
-# PROXY (OBRIGATÓRIO NO RAILWAY)
-# =========================
-
-app.add_middleware(
-    ProxyHeadersMiddleware,
-    trusted_hosts="*"
-)
-
-# =========================
-# CORS (RAILWAY FRONTEND)
-# =========================
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,15 +16,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# =========================
-# DATABASE
-# =========================
-
 Base.metadata.create_all(bind=engine)
-
-# =========================
-# ROTAS
-# =========================
 
 app.include_router(salons.router)
 app.include_router(clientes.router)
@@ -46,11 +24,6 @@ app.include_router(servicos.router)
 app.include_router(agendamentos.router)
 app.include_router(auth.router)
 
-# =========================
-# ROOT
-# =========================
-
 @app.get("/")
 def root():
     return {"message": "Horvex API running"}
-
