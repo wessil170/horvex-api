da3a650bb600525ffa7a523a7b4bf7f546543ee3
Wesley Pereira <wessil170@gmail.com>
2026-02-19 15:35:47 -0300

    servicos multi-tenant protegido

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://horvex-web-production.up.railway.app"
+        "https://horvex-web.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
